5f6a7b9de250261210e6a33be6ad406b3f4e0e9b
TouchingGrassEnjoyer <georgepetpet@gmail.com>
2023-10-15 17:35:44 +0300

    ADDED SOME CODE ON C JUST FOR A GIT DEMO

---
+++ scanf.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void printAll();
+
+//########################################################################
+
+int main(){
+
+char buffer[50];
+int temp;
+
+    printf("Give string: ");
+
+    temp = scanf("%[abc]",buffer);
+
+    printf("\n:%s",bufer);
+
+    printf("\n:%i",temp);
+
+    printAll();
+
+return 0;
+}
+
+//########################################################################
+
+void printAll(){
+
+printf("TIME TO HACK EM ALL!");
+
+
+}
+
